878176b13d453ead0a55aaafbfd0d6c801f4f617
ZiugatWong <ziugat.wong@gmail.com>
2025-07-25 13:25:37 +0800

    docs: upload block lists

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # user-block-list
-block any users in any website for myself
+
+自用的各网站用户屏蔽名单，仅代表我的个人喜好
+
+## 屏蔽插件汇总
+
+|网站|插件|
+|:---:|:---:|
+|youtube|[BlockTube](https://chromewebstore.google.com/detail/blocktube/bbeaicapbccfllodepmimpkgecanonai)|
+|b站|[bilibili页面净化大师](https://greasyfork.org/en/scripts/479861-bilibili-%E9%A1%B5%E9%9D%A2%E5%87%80%E5%8C%96%E5%A4%A7%E5%B8%88)|
+|mobile01|[Mobile01會員標籤](https://greasyfork.org/en/scripts/539101-mobile01-%E6%9C%83%E5%93%A1%E6%A8%99%E7%B1%A4)|
---
+++ list/mobile01.js
@@ -0,0 +1,111 @@
+{
+    "tags": [
+        {
+            "name": "元老",
+            "color": "#df16b3"
+        },
+        {
+            "name": "輪子功",
+            "color": "#ac8806"
+        },
+        {
+            "name": "紅外宣",
+            "color": "#ad1414"
+        },
+        {
+            "name": "直．蛙",
+            "color": "#22a03b"
+        },
+        {
+            "name": "串．蛙",
+            "color": "#22a03b"
+        },
+        {
+            "name": "偷．蛙",
+            "color": "#22a03b"
+        }
+    ],
+    "userTags": {
+        "輪子功": [
+            "明宇",
+            "vn51",
+            "andye",
+            "Ian S."
+        ],
+        "元老": [
+            "ebola01",
+            "29x5x5x2"
+        ],
+        "直．蛙": [
+            "北一女小綠綠",
+            "2好3壞",
+            "ebola01",
+            "Disturbia",
+            "eanck",
+            "漲翻天",
+            "skistosais",
+            "stever2018",
+            "橫空出世",
+            "farmer3000",
+            "Rembo464",
+            "MINE 桑",
+            "PainterJ",
+            "29x5x5x2",
+            "選舉",
+            "Harvey norman",
+            "六月牛車",
+            "exia1086",
+            "stock6688",
+            "godsped",
+            "nt24759",
+            "voltes0",
+            "單車李",
+            "kenyeh99",
+            "cougar2015",
+            "Rodigital",
+            "KUMAPIPIPI",
+            "A辣愛怕跑",
+            "IDF302",
+            "愛。回。家",
+            "YC_Jiang",
+            "pw5stf",
+            "ajuggrutaiwan",
+            "i12345",
+            "你說的對",
+            "五毛獵人",
+            "LIKE-SEIKO",
+            "kikiki13579",
+            "alexlin1",
+            "lordkon",
+            "jasonwu709",
+            "Daniel Wen",
+            "melkavam",
+            "毛蛆只能吃屎",
+            "ggggxxxx26",
+            "kumapipipipi",
+            "trooper62",
+            "TONY1978",
+            "sunnyup",
+            "mitsu",
+            "肉圓沒加辣",
+            "yume4523",
+            "booktree0618",
+            "flin0310",
+            "adiouthg",
+            "b01"
+        ],
+        "串．蛙": [
+            "going_down"
+        ],
+        "偷．蛙": [
+            "MICHAELCUB81"
+        ],
+        "紅外宣": [
+            "Henryws168",
+            "江湖問路不問心",
+            "ok001",
+            "摘星子",
+            "alanchentw"
+        ]
+    }
+}
